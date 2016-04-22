19c0159e057e7f0f200ad507069a2c716f2e5129
Stephen Done <srdone@gmail.com>
2016-04-21 21:31:17 -0600

    add polling

--- examples_earthquake/code.js
+++ examples_earthquake/code.js
@@ -1,17 +1,24 @@
-var quakes = Rx.DOM.jsonpRequest({
-    url: QUAKE_URL,
-    jsonpCallback: 'eqfeed_callback'
-})
-.flatMap(function (dataset) {
-    return Rx.Observable.fromArray(dataset.response.features);
-})
-.map(function (quake) {
-    return {
-        lat: quake.geometry.coordinates[1],
-        lng: quake.geometry.coordinates[0],
-        size: quake.properties.mag * 1000
-    };
-});
+var quakes = Rx.Observable
+    .interval(5000)
+    .flatMap(function () {
+        return Rx.DOM.jsonpRequest({
+            url: QUAKE_URL,
+            jsonpCallback: 'eqfeed_callback'
+        }).retry(3);
+    })
+    .flatMap(function (dataset) {
+        return Rx.Observable.fromArray(dataset.response.features);
+    })
+    .distinct(function (quake) {
+        return quake.properties.code;
+    })
+    .map(function (quake) {
+        return {
+            lat: quake.geometry.coordinates[1],
+            lng: quake.geometry.coordinates[0],
+            size: quake.properties.mag * 1000
+        };
+    });
 
 quakes.subscribe(function (quake) {
     L.circle([quake.lat, quake.lng], quake.size).addTo(map);
